dcfb4746285645d93211ad2a18686229029bcd39
EgaPavanSaiVarshith <chintit401@gmail.com>
2026-02-04 01:17:19 +0530

    Leraned Variables concepts for loop & arrays concept

---
+++ variable.py
@@ -0,0 +1,22 @@
+lnt = [3,12,5,6,7]
+for i in  range (5):
+    print(lnt[i])
+
+
+lnt = [3,12,5,6,7]
+for i in  range (5):
+    print(lnt[i])
+
+
+int  = [1,2,3,4,5,6,7,8,9,10]
+print(int[0])
+print(int[2])
+print(int[4])
+print(int[6])
+print(int[8])
+
+
+
+orey = ["orey maccha","okkadu","re-release","anta ra"]
+for i in range(4):
+    print(orey[i])
