781f51f400f3c243577a03d8ce87ee1a55bcb45a
EgaPavanSaiVarshith <chintit401@gmail.com>
2026-02-15 22:21:15 +0530

    Added

---
+++ copymethod.py
@@ -0,0 +1,5 @@
+#copy() method is a built-in method in Python that creates a shallow copy of a list. This method is simple and highly efficient for cloning a list.
+
+a = [1, 2, 3, 4, 5]
+b = a.copy()
+print(b)
