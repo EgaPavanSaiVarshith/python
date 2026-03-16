43b4abb7a928c2796577b1ef78df38401ac4f485
EgaPavanSaiVarshith <chintit401@gmail.com>
2026-03-17 00:09:06 +0530

    Finding words after the space

--- doubleforloops.py
+++ doubleforloops.py
@@ -1,6 +1,7 @@
-li = [ 6 , 8 , 5 , 10 , 2]
-for i in li:
-    print(i,"Outside Loop")
-    for j in li:
-        print("Inside Loop")
-    print(i,"Outside exiting")
+s ="one problem a day keeps the doctor away"
+temp = 0
+for i in range(len(s)):
+    ch = s[i]
+    if ch == " ":
+        temp = temp + 1
+print(temp+1)
