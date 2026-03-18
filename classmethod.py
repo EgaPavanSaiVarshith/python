4f1f8ef89c9ac45df762b00a666ca66189b03e20
PAVAN SAI VARSHITH EGA <chintit401@gmail.com>
2026-03-18 11:57:13 +0530

    Add Mobile class with initialization and example object

---
+++ classmethod.py
@@ -0,0 +1,9 @@
+class Mobile:
+    def __init__(self, model, camera):
+        self.model = model
+        self.camera= camera
+
+mobile_obj = Mobile(
+    "iPhone 12 Pro",
+    "12 MP")
+print(mobile_obj)
