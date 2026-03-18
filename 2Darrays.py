eda5e0d461da249f06fe58a81489daa6639d0425
EgaPavanSaiVarshith <chintit401@gmail.com>
2026-03-19 01:06:01 +0530

    r as rows and c as columns we can add short form to add for 2d arrays

--- 2Darrays.py
+++ 2Darrays.py
@@ -1,4 +1,6 @@
-mat=[[3,4,5],[6,7,8],[9,10,11]]
-for i in range(3):
-    for j in range(3):
-        print(mat[i][j]) 
+mat=[[3,4,5],[6,7,8],[9,10,11],[4,6,8]]
+
+r = len(mat)
+print(r)
+c=len(mat[0])
+print(c)
