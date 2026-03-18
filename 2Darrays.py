46672e63be7e246411affa08fd01d37d6b46c8b2
EgaPavanSaiVarshith <chintit401@gmail.com>
2026-03-19 01:32:08 +0530

    tsk done

--- 2Darrays.py
+++ 2Darrays.py
@@ -1,7 +1,7 @@
-mat=[[1,2,3,4],[5,6,7,8]] 
+mat=[[1,2,3,4],[5,6,7,8],[18,2,3,4]] 
 
 r = len(mat)
 c=len(mat[0])
 
-for j in range(4):
-    print(mat[0][j])
+for i in range(r):
+    print(mat[i][3])
