8e328590336367958935ff863c2e9954086ce8a0
EgaPavanSaiVarshith <chintit401@gmail.com>
2026-04-05 22:54:05 +0530

    Added

--- practise1.py
+++ practise1.py
@@ -38,7 +38,7 @@ r=10
 c=3
 for i in range(r):
     for j in range(c):
-        print("*",end= "-")
-        # if j!=c-1:
-        #     print("-",end="")
+        print("*",end= "")
+        if j!=c-1:
+            print("-",end="")
     print()
