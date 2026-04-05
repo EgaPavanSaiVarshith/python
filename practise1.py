5c5ee8f406bf0ddcd85d0c391fea382a300bda10
EgaPavanSaiVarshith <chintit401@gmail.com>
2026-04-05 22:10:15 +0530

    to print ll the statements in one line

--- practise1.py
+++ practise1.py
@@ -1,2 +1,6 @@
-print("Hello",end = "\n")
-print("Pavan")
+# print("Hello",end = "\n")
+#  print("Pavan")
+
+print("Hello",end = " 1 ")
+print("Pavan",end = " 2 ")
+print("Boom Boom",end=" Bumrah ")
