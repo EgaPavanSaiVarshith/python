7329f7d970fed066113498fb39dbfe806345b134
EgaPavanSaiVarshith <chintit401@gmail.com>
2026-04-05 22:49:55 +0530

    Added

--- practise1.py
+++ practise1.py
@@ -29,7 +29,7 @@ r=3
 c=10
 for i in range(r):
     for j in range(c):
-        print("*",end= "-")
-        # if j!=c-1:
-        #     print("-",end="")
+        print("*",end= "")
+        if j!=c-1:
+            print("-",end="")
     print()
