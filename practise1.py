cebb02fc9ede28feef9f1793c0a57933b497b7de
EgaPavanSaiVarshith <chintit401@gmail.com>
2026-04-05 22:43:32 +0530

    Merge remote changes

--- practise1.py
+++ practise1.py
@@ -1,24 +1,24 @@
-# # # # print("Hello",end = "\n")
-# # # #  print("Pavan")
+# # # # # print("Hello",end = "\n")
+# # # # #  print("Pavan")
 
-# # # print("Hello",end = " 1 ")
-# # # print("Pavan",end = " 2 ")
-# # # print("Boom Boom",end=" Bumrah ")
+# # # # print("Hello",end = " 1 ")
+# # # # print("Pavan",end = " 2 ")
+# # # # print("Boom Boom",end=" Bumrah ")
 
-# # li = ["hello","pavan","boom"]
-# # for i in range(len(li)):
-# #     print(li[i],end = " ")
+# # # li = ["hello","pavan","boom"]
+# # # for i in range(len(li)):
+# # #     print(li[i],end = " ")
+
+# # r=5
+# # c=5
+# # for i in range(r):
+# #     for j in range(c):
+# #         print("*",end= " ")
+# #     print()
 
 # r=5
 # c=5
 # for i in range(r):
 #     for j in range(c):
-#         print("*",end= " ")
+#         print("*",end= "-")
 #     print()
-
-r=5
-c=5
-for i in range(r):
-    for j in range(c):
-        print("*",end= "-")
-    print()
