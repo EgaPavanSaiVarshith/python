ea9bc5ba44a9781eeb34a6bf4a4cc40fa1f3bc2d
EgaPavanSaiVarshith <chintit401@gmail.com>
2026-04-06 23:28:47 +0530

    Added

--- practise1.py
+++ practise1.py
@@ -53,13 +53,13 @@ c=3
 #     print()
 
 r=5
-c=4
+c=5
 for i in range(r):
     for j in range(c):
         if i==0 or i==r-1 or j==0 or j==c-1:
             print("*",end = "")
         else:
             print(" ",end = "")
-        if j!=c-1:
-            print(" ",end="")
+        # if j!=c-1:
+        #     print(" ",end="")
     print()
