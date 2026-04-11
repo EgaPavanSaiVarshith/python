31601fe19ca4fe4070bd0ff32c9967f35c69f396
EgaPavanSaiVarshith <chintit401@gmail.com>
2026-04-11 21:21:02 +0530

    Merge remote changes

--- practise1.py
+++ practise1.py
@@ -186,10 +186,10 @@ n=r-1
 for i in range(r-1):
     for j in range(n-i):
         print(" ",end="")
-    for j in range(i*2+1):
+    for j in range(i*2+2):
         print("*",end="")
     print()
-for i in range(r-1,-1,-1):
+for i in range(r-1,-1,-1,):
     for j in range(n-i):
         print(" ",end="")
     for j in range(i*2+1):
